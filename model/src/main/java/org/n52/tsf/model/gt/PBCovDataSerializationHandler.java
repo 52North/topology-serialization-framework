30f2af2d46242df6b4f8a4df02a822ab4a7e3fdd
malithd <mmalithh@gmail.com>
2017-07-10 02:05:09 +0200

    code refactoring for PBCovDataSerializationHandler

--- model/src/main/java/org/n52/tsf/model/gt/PBCovDataSerializationHandler.java
+++ model/src/main/java/org/n52/tsf/model/gt/PBCovDataSerializationHandler.java
@@ -1,3 +1,22 @@
+//
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+//
+
 package org.n52.tsf.model.gt;
 
 import org.geotools.coverage.grid.GridCoverage2D;
@@ -20,7 +39,7 @@ import java.io.OutputStream;
 /**
  * This class provides the Geotools coverage data to ProtoBuf Serialization functionality.
  */
-public class PBCovDataSerializationHadler {
+public class PBCovDataSerializationHandler {
 
     public void serialize(File geotifFile, OutputStream outputStream) throws Exception {
         GeoProtobufCov.GridWithCRS.Builder gridBuilder = GeoProtobufCov.GridWithCRS.newBuilder();
@@ -77,5 +96,6 @@ public class PBCovDataSerializationHadler {
         gridBuilder.setMaxWidth(maxDimensions.getCoordinateValue(0) + 1);
         gridBuilder.setMaxHight(maxDimensions.getCoordinateValue(1) + 1);
         gridBuilder.setSourceCrs(CRS.lookupIdentifier(coverage.getCoordinateReferenceSystem(), true));
+        gridBuilder.setColorSpace(coverage.getRenderedImage().getColorModel().getColorSpace().getType());
     }
 }
