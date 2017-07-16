9cde6a418328c7df624a875be0044a2422d908bf
malithd <mmalithh@gmail.com>
2017-07-16 19:09:20 +0200

    adding sample code for creating Mathtransform model from wordfile data

--- scratch/src/main/java/de/n52/tsf/scratch/geotools/WordFileReaderSample.java
+++ scratch/src/main/java/de/n52/tsf/scratch/geotools/WordFileReaderSample.java
@@ -1,6 +1,9 @@
 package de.n52.tsf.scratch.geotools;
 
 import org.geotools.data.WorldFileReader;
+import org.geotools.referencing.operation.matrix.GeneralMatrix;
+import org.geotools.referencing.operation.transform.ProjectiveTransform;
+import org.opengis.referencing.operation.MathTransform;
 
 import java.io.File;
 import java.io.IOException;
@@ -19,5 +22,19 @@ public class WordFileReaderSample {
         System.out.println(worldFileReader.getYPixelSize());
         System.out.println(worldFileReader.getRotationX());
         System.out.println(worldFileReader.getRotationY());
+
+        MathTransform mt = createMathtransform(worldFileReader);
+        System.out.println(mt.toWKT());
+    }
+
+    public static MathTransform createMathtransform(WorldFileReader worldFileReader){
+        GeneralMatrix gm = new GeneralMatrix(3);
+        gm.setElement(0, 0, worldFileReader.getXPixelSize());
+        gm.setElement(1, 1, worldFileReader.getYPixelSize());
+        gm.setElement(0, 1, worldFileReader.getRotationX());
+        gm.setElement(1, 0, worldFileReader.getRotationY());
+        gm.setElement(0, 2, worldFileReader.getXULC());
+        gm.setElement(1, 2, worldFileReader.getYULC());
+        return ProjectiveTransform.create(gm);
     }
 }
